45ab888c2fa2691786b709c3f901dc2af785c09b
Emil Hägglöv <emil.hagglov@yaelev.se>
2023-12-13 17:26:21 +0100

    Refactor GameGrid component to use custom hook for fetching games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-import { Text, list } from '@chakra-ui/react';
-
-interface Game {
-    id: number;
-    name: string;
-}
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-}
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [game, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
+  const { games, error } = useGames();
 
-  useEffect(() => {
-    apiClient.get<FetchGamesResponse>('/gamesx')
-    .then(response => setGames(response.data.results))
-    .catch(error => setError(error.message));
-  })
-  
-    return (
-        <>
-        {error && <Text>{error}</Text>}
-    <ul>
-        {game.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
