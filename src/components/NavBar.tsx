986a23827834e9a2250b5ff3915c416b9eb90521
Emil Hägglöv <emil.hagglov@yaelev.se>
2023-12-14 14:42:36 +0100

    Add search functionality to NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SerachInput from './SerachInput';
 
-const NavBar = () => {
+interface Props {
+    onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding='10px'>
         <Image src={logo} boxSize='60px'/>
-        <SerachInput />
+        <SerachInput onSearch={onSearch} />
         <ColorModeSwitch />
     </HStack>
   )
